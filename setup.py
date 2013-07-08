893e1d3ae2580c8d35fc484d37769232839a3f62
Trevor Bramwell <trevor@bramwell.net>
2013-07-08 08:48:19 -0700

    Fill out setup.py and MANIFEST.in
    Looks like the suspenders just got put on.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "numerals",
+    version = "0.0.0",
+    url = "http://github.com/bramwelt/numerals",
+    author = "Trevor Bramwell",
+    author_email = "trevor@bramwell.net",
+)
