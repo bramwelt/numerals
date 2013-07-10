dfdc64dbcd0ba8964d126c5ea38b53cb2aa99207
Trevor Bramwell <trevor@bramwell.net>
2013-07-09 23:49:52 -0700

    Stub for to_numeral and basic test as well

--- numerals/numerals.py
+++ numerals/numerals.py
@@ -8,3 +8,10 @@ def to_integer(numeral):
     Given a Roman numeral, convert it to an integer.
     """
     return int(5)
+
+
+def to_numeral(integer):
+    """
+    Given an integer, convert it to a Roman numeral.
+    """
+    return 'V'
--- numerals/tests/to_numeral.py
+++ numerals/tests/to_numeral.py
@@ -1,1 +1,15 @@
 import unittest
+
+from numerals import to_numeral
+
+
+class NumeralTestCase(unittest.TestCase):
+    """
+    Tests related to converting integers to numerals
+    """
+
+    def test_five(self):
+        """
+        'V' == 5
+        """
+        self.assertEqual('V', to_numeral(5))
