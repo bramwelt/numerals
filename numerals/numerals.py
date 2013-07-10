f971488a5127fe657c393afdf73d2740a4a4328a
Trevor Bramwell <trevor@bramwell.net>
2013-07-09 23:48:28 -0700

    Comment to_integer function in numerals

--- numerals/numerals.py
+++ numerals/numerals.py
@@ -4,4 +4,7 @@ Numerals is a package for converting to and from Roman numerals.
 
 
 def to_integer(numeral):
+    """
+    Given a Roman numeral, convert it to an integer.
+    """
     return int(5)
